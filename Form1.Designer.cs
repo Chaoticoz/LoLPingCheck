38d0eda6a8296e9af029489891c07229975d92ae
thomas <thomas.klemenc@gmail.com>
2020-04-30 10:47:01 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace LoLPingCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPing = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // lblPing
+            // 
+            this.lblPing.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPing.Location = new System.Drawing.Point(7, 37);
+            this.lblPing.Name = "lblPing";
+            this.lblPing.Size = new System.Drawing.Size(56, 27);
+            this.lblPing.TabIndex = 2;
+            this.lblPing.Text = "label1";
+            this.lblPing.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.lblPing.Click += new System.EventHandler(this.lblPing_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(85, 47);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(33, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "mpgh";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(130, 76);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblPing);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "LoL Ping Check";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPing;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoLPingCheck
+{
+    public partial class Form1 : Form
+    {
+        private Thread t;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            this.TopMost = true;
+        }
+        private static Dictionary<string, string> servers = new Dictionary<string, string>()
+        {
+          {
+            "EUW",
+            "104.160.141.3"
+          },
+          {
+            "NA",
+            "104.160.131.3"
+          },
+          {
+            "EUNE",
+            "104.160.142.3"
+          },
+          {
+            "OCE",
+            "104.160.156.1"
+          },
+          {
+            "LAN",
+            "104.160.136.3"
+          },
+          {
+            "Garena",
+            "203.117.172.253"
+          },
+          {
+            "BR",
+            "104.160.152.3"
+          }
+        };
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("EUW");
+            comboBox1.Items.Add("EUNE");
+            comboBox1.Items.Add("NA");
+            comboBox1.Items.Add("OCE");
+            comboBox1.Items.Add("Garena");
+            comboBox1.Items.Add("LAN");
+            comboBox1.Items.Add("BR");
+            comboBox1.SelectedIndex = Properties.Settings.Default.Server;
+            lblPing.Text = "Pinging..";
+            lblPing.ForeColor = Color.Black;
+            t = new Thread(new ThreadStart(this.pingServer));
+            t.Start();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Server = comboBox1.SelectedIndex;
+            Properties.Settings.Default.Save();
+        }
+
+        private void pingServer()
+        {
+            int TTL = 64;
+            int timeout = 1000;
+            
+
+            Ping pingSender = new Ping();
+            byte[] buffer = new byte[32];
+            PingOptions options = new PingOptions(TTL, true);
+            while (true)
+            {
+                string server = getServer();
+                PingReply reply = pingSender.Send(server, timeout, buffer, options);
+                if (reply.Status == IPStatus.Success)
+                {
+                    lblPing.Text = reply.RoundtripTime.ToString();
+                    lblPing.ForeColor = Color.Green;
+                    if (reply.RoundtripTime > 50) { lblPing.ForeColor = Color.Orange; }
+                    if (reply.RoundtripTime > 100) { lblPing.ForeColor = Color.Red; }
+                }
+                else
+                {
+                    lblPing.Text = "???";
+                }
+                Thread.Sleep(250);
+            }
+        }
+        public string getServer()
+        {
+            return servers[comboBox1.Text];
+        }
+
+        private void btnPing_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPing_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            e.Link.LinkData = (object)"http://www.mpgh.net/";
+            string fileName = e.Link.LinkData == null ? this.linkLabel1.Text.Substring(e.Link.Start, e.Link.Length) : e.Link.LinkData.ToString();
+            if (!fileName.Contains("://"))
+                fileName = "http://" + fileName;
+            Process.Start(new ProcessStartInfo(fileName));
+            this.linkLabel1.LinkVisited = true;
+        }
+    }
+}
